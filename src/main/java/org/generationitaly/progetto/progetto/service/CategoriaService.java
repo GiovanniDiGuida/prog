6e2c533a6e0ec5afd87f246d0fd1d2d0795aa527
Dominic <118680299+bjdome@users.noreply.github.com>
2025-03-13 09:32:20 +0100

    aggiunta categoriaController

--- src/main/java/org/generationitaly/progetto/progetto/service/CategoriaService.java
+++ src/main/java/org/generationitaly/progetto/progetto/service/CategoriaService.java
@@ -2,12 +2,14 @@ package org.generationitaly.progetto.progetto.service;
 
 import java.util.List;
 
-
 import org.generationitaly.progetto.progetto.entity.Categoria;
-
+import org.generationitaly.progetto.progetto.entity.Programma;
 import org.generationitaly.progetto.progetto.repo.CategoriaRepo;
+import org.generationitaly.progetto.progetto.repo.ProgrammaRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class CategoriaService {
@@ -15,23 +17,34 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepo categoriaRepo;
 
+    @Autowired
+    private ProgrammaRepo programmaRepo;
 
-
-
-
-    public List<Categoria> findall(){
+    public List<Categoria> findall() {
         return categoriaRepo.findAll();
     }
 
-    public Categoria findById(Long id){
+    public Categoria findById(Long id) {
         return categoriaRepo.findById(id).orElse(null);
     }
 
-    public void deleteById(Long id){
+    public void deleteById(Long id) {
         categoriaRepo.deleteById(id);
     }
 
-    public void delete(Categoria categoria){
+    @Transactional
+    public void deleteCategoria(Long id) {
+        Categoria categoria = categoriaRepo.findById(id).orElse(null);
+
+        // Rimuove il canale da tutti i programmi associati
+        for (Programma programma : categoria.getProgrammi()) {
+            programma.getCategorie().remove(categoria);
+        }
+
+        // Salva i programmi per aggiornare il DB
+        programmaRepo.saveAll(categoria.getProgrammi());
+
+        // Ora puoi eliminare il canale
         categoriaRepo.delete(categoria);
     }
 
